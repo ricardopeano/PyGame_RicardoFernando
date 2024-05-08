7506e91409b9b268ec8a60b59a70b19512cd2515
ricardopeano <ricardocp8@al.insper.edu.br>
2024-05-08 09:13:09 -0300

    implementacao de alguns assets e configs

---
+++ assets.py
@@ -0,0 +1,29 @@
+import pygame
+import os
+from config import IMG_DIR, SND_DIR, FNT_DIR, DINO_PE_WIDTH, DINO_PE_HEIGHT, DINO_AGA_WIDTH, DINO_AGA_HEIGHT
+
+BACKGROUND = 'background'
+DINO_PE_IMG = 'dino_pe_img'
+DINO_AGACHADO_IMG = 'dino_agachado_img'
+OBS1_IMG = 'obs1_img'
+SCORE_FONT = 'score_font'
+JUMP_SOUND = 'jump_sound'
+DEATH_SOUND = 'death_sound'
+BACKGROUND_SOUND = 'background_sound'
+
+def load_assets():
+    assets = {}
+    assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR, 'background_dino.jpeg')).convert()
+    assets[DINO_PE_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'de_pe.png')).convert_alpha()
+    assets[DINO_PE_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[DINO_AGACHADO_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'agachado.png')).convert_alpha()
+    assets[DINO_AGACHADO_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[SCORE_FONT] = pygame.font.Font(os.path.join(FNT_DIR, 'scorefont.TTF'), 28)
+
+    # Carrega os sons do jogo
+    pygame.mixer.music.load(os.path.join(SND_DIR, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+    pygame.mixer.music.set_volume(0.4)
+    assets[BOOM_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl3.wav'))
+    assets[DESTROY_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl6.wav'))
+    assets[PEW_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'pew.wav'))
+    return assets
---
+++ config.py
@@ -0,0 +1,30 @@
+from os import path
+
+# Estabelece a pasta que contem as figuras e sons.
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'images')
+SND_DIR = path.join(path.dirname(__file__), 'assets', 'sounds')
+FNT_DIR = path.join(path.dirname(__file__), 'assets', 'fonts')
+
+# Dados gerais do jogo.
+WIDTH = 800 # Largura da tela
+HEIGHT = 400 # Altura da tela
+FPS = 60 # Frames por segundo
+
+# Define tamanhos
+DINO_PE_WIDTH = 0
+DINO_PE_HEIGHT = 0
+DINO_AGA_WIDTH = 0
+DINO_AGA_HEIGHT = 0
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Estados para controle do fluxo da aplicação
+INIT = 0
+GAME = 1
+QUIT = 2
